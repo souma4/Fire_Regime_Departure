73085d0778a2591e4926eb2a4c1827cca53d44ef
souma4 <jeffrey.chandler435@gmail.com>
2024-01-19 18:05:49 -0700

    A valid diverging color plot. may try a 5 x 5 grid at some point, idk

--- Scripts/figure_generation/R_plots.r
+++ Scripts/figure_generation/R_plots.r
@@ -59,52 +59,87 @@ theme_bw()+
 
 #diverging color ramp
 ## create color matrix
-colmat<-function(nquantiles=10, upperleft=rgb(0,150,235, maxColorValue=255), upperright=rgb(130,0,80, maxColorValue=255), bottomleft="grey", bottomright=rgb(255,230,15, maxColorValue=255), xlab="x label", ylab="y label"){
-  my.data<-seq(0,1,.01)
-  my.class<-classIntervals(my.data,n=nquantiles,style="quantile")
-  my.pal.1<-findColours(my.class,c(upperleft,bottomleft))
-  my.pal.2<-findColours(my.class,c(upperright, bottomright))
-  col.matrix<-matrix(nrow = 101, ncol = 101, NA)
-  for(i in 1:101){
-    my.col<-c(paste(my.pal.1[i]),paste(my.pal.2[i]))
-    col.matrix[102-i,]<-findColours(my.class,my.col)}
-  plot(c(1,1),pch=19,col=my.pal.1, cex=0.5,xlim=c(0,1),ylim=c(0,1),frame.plot=F, xlab=xlab, ylab=ylab,cex.lab=1.3)
-  for(i in 1:101){
-    col.temp<-col.matrix[i-1,]
-    points(my.data,rep((i-1)/100,101),pch=15,col=col.temp, cex=1)}
-  seqs<-seq(0,100,(100/nquantiles))
-  seqs[1]<-1
-  col.matrix<-col.matrix[c(seqs), c(seqs)]}
-
-
-bivariate.map<-function(rasterx, rastery, colormatrix=col.matrix, nquantiles=10){
-  quanmean<-values(rasterx)
-  temp<-data.frame(quanmean, quantile=rep(NA, length(quanmean)))
-  brks<-with(temp, quantile(temp,na.rm=TRUE, probs = c(seq(0,1,1/nquantiles))))
-  r1<-within(temp, quantile <- cut(quanmean, breaks = brks, labels = 2:length(brks),include.lowest = TRUE))
-  quantr<-data.frame(r1[,2]) 
-  quanvar<-values(rastery)
-  temp<-data.frame(quanvar, quantile=rep(NA, length(quanvar)))
-  brks<-with(temp, quantile(temp,na.rm=TRUE, probs = c(seq(0,1,1/nquantiles))))
-  r2<-within(temp, quantile <- cut(quanvar, breaks = brks, labels = 2:length(brks),include.lowest = TRUE))
-  quantr2<-data.frame(r2[,2])
-  as.numeric.factor<-function(x) {as.numeric(levels(x))[x]}
-  col.matrix2<-colormatrix
-  cn<-unique(colormatrix)
-  for(i in 1:length(col.matrix2)){
-    ifelse(is.na(col.matrix2[i]),col.matrix2[i]<-1,col.matrix2[i]<-which(col.matrix2[i]==cn)[1])}
-  cols<-numeric(length(quantr[,1]))
-  for(i in 1:length(quantr[,1])){
-    a<-as.numeric.factor(quantr[i,1])
-    b<-as.numeric.factor(quantr2[i,1])
-    cols[i]<-as.numeric(col.matrix2[b,a])}
-  r<-rasterx
-  r[1:length(r)]<-cols
-  return(r)}
-
-
-
-col.matrix<-colmat(nquantiles=10, upperleft="#542788", upperright="#c51b7d", bottomleft="#b35806", bottomright="#4d9221", xlab="My x label", ylab="My y label")
+# colmat<-function(nquantiles=10, upperleft=rgb(0,150,235, maxColorValue=255),
+#                  upperright=rgb(130,0,80, maxColorValue=255), 
+#                  bottomleft="grey", bottomright=rgb(255,230,15, maxColorValue=255), 
+#                  diverging = NULL,
+#                  xlab="x label", ylab="y label"){
+#   my.data<-seq(0,1,.01)
+#   my.class<-classIntervals(my.data,n=nquantiles,style="quantile")
+#   
+#  
+#   if(!is.null(diverging)){
+#   my.pal.1<-findColours(my.class,c(upperleft,bottomleft))
+#   my.pal.2<-findColours(my.class,c(upperright, bottomright))
+#   
+#   
+#   col.matrix<-matrix(nrow = 101, ncol = 101, NA)
+#   for(i in 1:101){
+#     my.col<-c(paste(my.pal.1[i]),paste(my.pal.2[i]))
+#     
+#       col.matrix[102-i,]<-findColours(my.class,my.col)
+#       
+#     }
+#   plot(c(1,1),pch=19,col=my.pal.1, cex=0.5,xlim=c(0,1),ylim=c(0,1),frame.plot=F, xlab=xlab, ylab=ylab,cex.lab=1.3)
+#   for(i in 1:101){
+#     col.temp<-col.matrix[i-1,]
+#     points(my.data,rep((i-1)/100,101),pch=15,col=col.temp, cex=1)
+#     }
+#   seqs<-seq(0,100,(100/nquantiles))
+#   seqs[1]<-1
+#   col.matrix<-col.matrix[c(seqs), c(seqs)]
+#   } else{
+#     left_side <- findColours(my.class, c(upperleft, diverging$middle_left, bottomleft))
+#     right_side <- findColours(my.class, c(upperright, diverging$middle_right, bottomright))
+#     top_side <- findColours(my.class, c(upperleft, diverging$middle_top, upperright))
+#     bottom_side <- findColours(my.class, c(bottomleft, diverging$middle_bottom, bottomright))
+#     middle_column <- findColours(my.class, c(diverging$middle_bottom, diverging$middle, diverging$middle_top))
+#     middle_row <- findColours(my.class, c(diverging$middle_left, diverging$middle, diverging$middle_right))
+#     col.matrix <- matrix(nrow = 101, ncol = 101, NA)
+#     col.matrix[1,] <- top_side
+#     col.matrix[101,] <- bottom_side
+#     col.matrix[,1] <- left_side
+#     col.matrix[,101] <- right_side
+#     col.matrix[50,] <- middle_row
+#     col.matrix[,50] <- middle_column
+#   
+#   }
+#   }
+# 
+# diverging <- list(middle_left = "#FEE0B6",
+#                   middle_top = "#c2a5cf",
+#                   middle_right = "#fde0ef",
+#                   middle_bottom = "#d9f0d3",
+#                   middle = "#f7f7f7")
+# bivariate.map<-function(rasterx, rastery, colormatrix=col.matrix, nquantiles=10){
+#   quanmean<-values(rasterx)
+#   temp<-data.frame(quanmean, quantile=rep(NA, length(quanmean)))
+#   brks<-with(temp, quantile(temp,na.rm=TRUE, probs = c(seq(0,1,1/nquantiles))))
+#   r1<-within(temp, quantile <- cut(quanmean, breaks = brks, labels = 2:length(brks),include.lowest = TRUE))
+#   quantr<-data.frame(r1[,2]) 
+#   quanvar<-values(rastery)
+#   temp<-data.frame(quanvar, quantile=rep(NA, length(quanvar)))
+#   brks<-with(temp, quantile(temp,na.rm=TRUE, probs = c(seq(0,1,1/nquantiles))))
+#   r2<-within(temp, quantile <- cut(quanvar, breaks = brks, labels = 2:length(brks),include.lowest = TRUE))
+#   quantr2<-data.frame(r2[,2])
+#   as.numeric.factor<-function(x) {as.numeric(levels(x))[x]}
+#   col.matrix2<-colormatrix
+#   cn<-unique(colormatrix)
+#   for(i in 1:length(col.matrix2)){
+#     ifelse(is.na(col.matrix2[i]),col.matrix2[i]<-1,col.matrix2[i]<-which(col.matrix2[i]==cn)[1])}
+#   cols<-numeric(length(quantr[,1]))
+#   for(i in 1:length(quantr[,1])){
+#     a<-as.numeric.factor(quantr[i,1])
+#     b<-as.numeric.factor(quantr2[i,1])
+#     cols[i]<-as.numeric(col.matrix2[b,a])}
+#   r<-rasterx
+#   r[1:length(r)]<-cols
+#   return(r)}
+# 
+# 
+#col.matrix<-colmat(nquantiles=10)
+#col.matrix<-colmat(nquantiles=10, upperleft="#5b4da1", upperright="#0d8943", bottomleft="#ea0d7f", bottomright="#f07621",
+#                   xlab="My x label", ylab="My y label")
 
 
 
@@ -128,14 +163,42 @@ df_joined_div <- df_joined %>%
                                            color_ramp_freq_code == 0 & color_ramp_sev_code == -1 ~ "#d9f0d3",
                                            
                                            color_ramp_freq_code == 1 & color_ramp_sev_code == 0 ~ "#fde0ef",
-                                           color_ramp_freq_code == -1 & color_ramp_sev_code == 0 ~ "#fee0b6")) %>%
-         select(color_ramp_color)
+                                           color_ramp_freq_code == -1 & color_ramp_sev_code == 0 ~ "#fee0b6"),
+         b = (signed_emd_frequency_normalized + max(emd_frequency_normalized))/(2*max(emd_frequency_normalized)),
+         r = (signed_emd_severity_normalized + max(emd_severity_normalized))/(2*max(emd_severity_normalized)),
+         g = 0.5,
+         rgb = rgb(r,g, -(b-1))) %>%
+         select(color_ramp_color, rgb)
 diverging_departure_plot <- ggplot() +
   geom_sf(data = states_df, fill = "grey90", color = "black") +
   geom_sf(data = df_joined_div, fill = df_joined_div$color_ramp_color, linewidth = 0) +
   theme_bw()
 diverging_departure_plot
+
+diverging_departure_continuous <- ggplot() +
+  geom_sf(data = df_joined_div, aes(fill = rgb),linewidth = 0)+
+  scale_fill_identity()+
+  theme_bw()
+diverging_departure_continuous
 ggsave(paste0(outpath,"/main/diverging_departure_map.jpg"), plot = diverging_departure_plot, width = 8, height = 6, units = "in")
+
+####color palette
+d <- expand.grid(x = seq(0, 1, 0.01), y = seq(0, 1, 0.01)) %>%
+  mutate(fill_val = atan(y/x),
+         transparency = x + y)
+
+d <- d %>%
+  mutate(mix2 = rgb(red = y, green = 0.5, blue = -(x-1)))
+ggplot(d, aes(x, y)) + 
+  geom_tile(aes(fill = mix2)) + 
+  labs(x = "",
+       y = "") +
+  scale_fill_identity()+
+  theme_void() +
+  theme(
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank()
+  )
 
 
 
